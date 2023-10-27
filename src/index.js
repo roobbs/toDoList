caa464aef0f2b7240bd05fa37848d942a1c2575c
Carlos <carlossalmeroncabrera@gmail.com>
2023-10-26 19:33:08 -0600

    added main files, all working as expected

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './style.css';
+console.log("This is working now");
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  
+  devtool: 'inline-source-map',
+
+  devServer: {
+    static: './dist',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'head',
+        scriptLoading: 'defer',
+    })
+  ],
+
+};
